015a7e323d8c2e36ec38ea85a6555843a6f553f0
A9250851 <79957632+A9250851@users.noreply.github.com>
2021-06-17 20:07:55 +0800

    Update main.cpp

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -9,12 +9,12 @@ int main()
 {
     int level;
     cout << "0 is test mod\n1 to 3 which level you want:";
-    cin >> level;
-    SudokuGen NewBoard; 
-    vector <int > board = NewBoard.board_Hole(level); 
-    NewBoard.print(board);
-    NewBoard.userCin(board);
-    vector <int > boardANS = NewBoard.board_Hole(99);
-    NewBoard.print(boardANS);
+    cin >> level;//難度
+    SudokuGen NewBoard; //產生棋盤物件
+    vector <int > board = NewBoard.board_Hole(level); //取得題目
+    NewBoard.print(board);//輸出題目
+    NewBoard.userCin(board);//輸入答案並檢查
+    vector <int > boardANS = NewBoard.board_Hole(99);//取得答案
+    NewBoard.print(boardANS);//輸出答案
 }
 
