aefc6bc211e4a67d1a59790cc244f41763ce31bb
A9250851 <79957632+A9250851@users.noreply.github.com>
2021-06-17 20:15:40 +0800

    Update SudokuGen.h

--- sudoku/SudokuGen.h
+++ sudoku/SudokuGen.h
@@ -11,15 +11,15 @@ class SudokuGen
 {
 public:
     SudokuGen();
-    void generator(); 
-    vector<int > board_Hole(int); 
-    bool check_rep(vector<int>, int, int); 
-    bool board_put(vector<int>&); 
-    void print(vector <int > board);
-    void userCin(vector <int > board);
+    void generator();//亂數法(產生新棋盤)
+    vector<int > board_Hole(int);//輸出有洞的棋盤至vector變數
+    bool check_rep(vector<int>, int, int);//偵測填入數字是否重複及是否符合規則，若不符合會回傳false值
+    bool board_put(vector<int>&);//填入數字函式，嘗試失敗超過1000次則會回傳false值
+    void print(vector <int > board);//輸出
+    void userCin(vector <int > board);//輸入答案並檢查
 private:
-    int new_board[SIZE][SIZE]; 
-    int i, j, k, l; 
-    int level;
+    int new_board[SIZE][SIZE];//棋盤
+    int i, j, k, l;//迴圈用變數
+    int level;//難度
 };
 #endif 
